3a56338233b3d2d9619a4758e755eb7a47784766
Thomaslam2142 <thomaslam2142@gmail.com>
2023-08-26 21:37:14 -0400

    replacing the question and options work now.

--- Quizgame.js
+++ Quizgame.js
@@ -1,13 +1,37 @@
-const question1= "What is a real html tag?";
+const question1 = "Which one is a real html tag?";
+const question2 = "Which one is a JavaScript Framework"
+const question3 = "Which is a valid input for js?"
 
 const q1ans1 = "<html>";
 const q1ans2 = "<horse>";
 
-let currentQuestion = document.getElementById("questionElement");
+const q2ans1 = "Kangaroo";
+const q2ans2 = "React";
+
+let questionNum = 0;
+
+
 
 function replaceText() {
     var element = document.getElementById("questionElement");
+    var btnOption1 = document.getElementById("optionbtn1");
+    var btnOption2 = document.getElementById("optionbtn2");
 
+
+    console.log(btnOption1);
     // Step 3: Use JavaScript to replace the text content of the selected element.
-    element.textContent = question1;
+    if (questionNum  == 0) {
+        element.textContent = question1;
+        btnOption1.textContent = q1ans1;
+        btnOption2.innerText = q1ans2;
+    }
+    if (questionNum  == 1) {
+        element.textContent = question2;
+    }
+    if (questionNum  == 2) {
+        element.textContent = question3;
+    }
+
+    questionNum += 1;
+
 }
