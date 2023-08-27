eaaf2a9d8782d6cd3d7047677d3658e6d7bd5b7e
Thomaslam2142 <thomaslam2142@gmail.com>
2023-08-26 21:10:08 -0400

    Can replace the question text now

--- Quizgame.js
+++ Quizgame.js
@@ -1,5 +1,13 @@
-const question1= "What is a real html tag"
+const question1= "What is a real html tag?";
 
-const q1ans1 = "<html>"
-const q1ans2 = "<horse>"
+const q1ans1 = "<html>";
+const q1ans2 = "<horse>";
 
+let currentQuestion = document.getElementById("questionElement");
+
+function replaceText() {
+    var element = document.getElementById("questionElement");
+
+    // Step 3: Use JavaScript to replace the text content of the selected element.
+    element.textContent = question1;
+}
