e361dd8ddde6278f5e6cc2381d8e566cf165e97c
Thomaslam2142 <thomaslam2142@gmail.com>
2023-08-26 21:46:10 -0400

    3 different questions implemented now

--- Quizgame.js
+++ Quizgame.js
@@ -1,12 +1,15 @@
 const question1 = "Which one is a real html tag?";
 const question2 = "Which one is a JavaScript Framework"
-const question3 = "Which is a valid input for js?"
+const question3 = "Which is a valid method invoke for js?"
 
 const q1ans1 = "<html>";
 const q1ans2 = "<horse>";
 
 const q2ans1 = "Kangaroo";
 const q2ans2 = "React";
+
+const q3ans1 = "function replaceText()";
+const q3ans2 = "meth replaceText()";
 
 let questionNum = 0;
 let correctCount = 0;
@@ -28,9 +31,13 @@ function replaceText() {
     }
     if (questionNum  == 1) {
         element.textContent = question2;
+        btnOption1.textContent = q2ans1;
+        btnOption2.innerText = q2ans2;
     }
     if (questionNum  == 2) {
         element.textContent = question3;
+        btnOption1.textContent = q3ans1;
+        btnOption2.innerText = q3ans2;
     }
 
     questionNum += 1;
